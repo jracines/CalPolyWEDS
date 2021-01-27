3a89705ce8ba354c32ab46c5331e6fd01fc06721
jracines <jeanrenoracines@gmail.com>
2021-01-26 18:35:41 -0800

    camera test at default resolution

--- camera/test.py
+++ camera/test.py
@@ -7,12 +7,12 @@ def main():
 
     camera.start_preview()
     # Default resolution: monitor resolution
-    camera.resolution = (2592, 1944)
+    # camera.resolution = (2592, 1944)
     # camera.resolution = (1920, 1080)
     camera.framerate = 15
     camera.rotation = 180
     sleep(5)
-    camera.capture('/home/pi/Desktop/sp/CalPolyWEDS/camera/pyCamTest3.jpg')
+    camera.capture('/home/pi/Desktop/sp/CalPolyWEDS/camera/pyCamTest.jpg')
     camera.stop_preview()
     # print("hello!")
 
