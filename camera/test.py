7f7da36c5804a02ffc919d649b6e97e63227dac7
jracines <jeanrenoracines@gmail.com>
2021-01-26 18:31:07 -0800

    camera test

--- camera/test.py
+++ camera/test.py
@@ -6,9 +6,12 @@ def main():
     camera = PiCamera()
 
     camera.start_preview()
+    # Default resolution: monitor resolution
+    camera.resolution = (2592, 1944)
+    camera.framerate = 15
     camera.rotation = 180
     sleep(5)
-    camera.capture('/home/pi/Desktop/sp/CalPolyWEDS/camera/pyCamTest.jpg')
+    camera.capture('/home/pi/Desktop/sp/CalPolyWEDS/camera/pyCamTest3.jpg')
     camera.stop_preview()
     # print("hello!")
 
