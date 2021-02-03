f8b83a56f081efe8c886c36708d5b81c846ed675
jracines <jeanrenoracines@gmail.com>
2021-02-02 16:04:32 -0800

    haar test

--- camera/objectDetection.py
+++ camera/objectDetection.py
@@ -20,11 +20,12 @@ def detectAndDisplay(frame):
             radius = int(round((w2 + h2)*0.25))
             frame = cv.circle(frame, eye_center, radius, (255, 0, 0 ), 4)
 
-    cv.imshow('Capture - Face detection', frame)
+    # cv.imshow('Capture - Face detection', frame)
+    cv.imwrite("haarout.jpg", frame)
 
 parser = argparse.ArgumentParser(description='Code for Cascade Classifier tutorial.')
-parser.add_argument('--face_cascade', help='Path to face cascade.', default='data/haarcascades/haarcascade_frontalface_alt.xml')
-parser.add_argument('--eyes_cascade', help='Path to eyes cascade.', default='data/haarcascades/haarcascade_eye_tree_eyeglasses.xml')
+parser.add_argument('--face_cascade', help='Path to face cascade.', default='opencv/data/haarcascades/haarcascade_frontalface_alt.xml')
+parser.add_argument('--eyes_cascade', help='Path to eyes cascade.', default='opencv/data/haarcascades/haarcascade_eye_tree_eyeglasses.xml')
 parser.add_argument('--camera', help='Camera divide number.', type=int, default=0)
 args = parser.parse_args()
 
