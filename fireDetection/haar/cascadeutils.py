cf7603d539737bda36c764726a32daae11e3faa6
jracines <jeanrenoracines@gmail.com>
2021-02-06 21:18:33 -0800

    new cascade classifier

--- fireDetection/haar/cascadeutils.py
+++ fireDetection/haar/cascadeutils.py
@@ -24,3 +24,5 @@ def generate_negative_description_file():
 #   HR: hit rate, FA: false alarm, N: weak layer number... detail increases with each layer
 #       Want the final layer to have the smallest FA rate possible without over training
 #   Overtraining: if NEG count : acceptanceRatio 100 : 0.0000... more than 4 zeros
+#
+#   C:/Users/Jayare/Downloads/opencv/build/x64/vc15/bin/opencv_traincascade.exe -data cascade/ -vec pos.vec -bg neg.txt -w 24 -h 24 -precalcValBufSize 6000 -precalcIdxBufSize 6000 -numPos 45 -numNeg 500 -numStages 12 -maxFalseAlarmRate 0.3 -minHitRate 0.999
