7bc7be9cf256267b44c3120ef696ed50f1ae3e3c
jeanrenoracines <jracines@evolvethefuture.org>
2021-03-29 15:33:49 -0700

    added backup folder for images

--- fireDetection/haar2/resizeImages.py
+++ fireDetection/haar2/resizeImages.py
@@ -1,10 +1,15 @@
 import cv2 as cv
+import glob
 
 def main():
+    # Read only .jpg files 
     filenames = glob.glob("positive/*.jpg")
     filenames.sort()
     images = [cv.imread(img) for img in filenames]
 
+    # Scaling dimensions
+
+    # Resize each image 
     for img in images:
         print(img)
 
