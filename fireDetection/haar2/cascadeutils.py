c7738a698e71efbade85a43f5e2ea49be1ddd304
jracines <jeanrenoracines@gmail.com>
2021-04-09 17:56:06 -0700

    75 stage

--- fireDetection/haar2/cascadeutils.py
+++ fireDetection/haar2/cascadeutils.py
@@ -28,7 +28,13 @@ def generate_negative_description_file():
 #   numNeg can be any number, some say it must be 1/2 or 2x the positive samples
 #   numStages beware of overtraining... 10 stages is good for 200 samples
 #   HR: hit rate, FA: false alarm, N: weak layer number... detail increases with each layer
+#       Layer 1, 2, ... are the outermost layers 
 #       Want the final layer to have the smallest FA rate possible without over training
 #   Overtraining: if NEG count : acceptanceRatio 100 : 0.0000... more than 4 zeros
+#
+#   Other parameters
+#       -precalcValBufSize:     gives more memory to help model train faster
+#       -maxFalseAlarmRate:     keeps adding more layers until this value is reached
+#       -minHitRate:            keeps adding more layers until this value is reached    
 #
 #   C:/Users/Jayare/Downloads/opencv/build/x64/vc15/bin/opencv_traincascade.exe -data cascade/ -vec pos.vec -bg neg.txt -w 24 -h 24 -precalcValBufSize 6000 -precalcIdxBufSize 6000 -numPos 45 -numNeg 500 -numStages 12 -maxFalseAlarmRate 0.3 -minHitRate 0.999
