b40c4e740de5eb38bd5a92babae9182e73a8753b
jracines <jeanrenoracines@gmail.com>
2021-04-19 18:20:14 -0700

    haar.py takes two command line arguments now. the first is for the cascade number and the second is for the footage number. this is to make testing easier

--- fireDetection/haar2/haar.py
+++ fireDetection/haar2/haar.py
@@ -5,6 +5,7 @@ from picamera.array import PiRGBArray
 from picamera import PiCamera
 from time import time
 import numpy as np
+import sys 
 
 def draw_rectangles(frame, rectangles):
     line_color = (0,255,0)
@@ -15,61 +16,68 @@ def draw_rectangles(frame, rectangles):
         cv.rectangle(frame, top_left, bottom_right, line_color, lineType=line_type)
     return frame
 
-parser = argparse.ArgumentParser(description='Code for Cascade Classifier tutorial.')
-parser.add_argument('--camera', help='Camera divide number.', type=int, default=0)
-args = parser.parse_args()
+def main():
+    if(len(sys.argv) < 3):
+        print('Usage: python3 haar.py <cascade num> <footage num>')
+        exit()
+    
+    cascade_num = sys.argv[1]
+    footage_num = sys.argv[2]
 
-camera_device = args.camera
+    # # -- 2. Read the video stream (Raspberry Pi Camera)
+    # cap = cv.VideoCapture(camera_device)
+    # if not cap.isOpened:
+    #     print('--(!)Error opening video capture')
+    #     exit(0)
 
-#-- 2. Read the video stream (Raspberry Pi Camera)
-# cap = cv.VideoCapture(camera_device)
-# if not cap.isOpened:
-#     print('--(!)Error opening video capture')
-#     exit(0)
+    # Reading test raw video of wildfire
+    footage_path = "footage/raw_footage_"+str(footage_num)+".mp4"
+    cap = cv.VideoCapture(footage_path)
 
-# Reading test raw video of wildfire
-cap = cv.VideoCapture("footage/raw_footage_023.mp4")
+    # Error checking
+    if not cap.isOpened():
+        print("Cannot open capture")
+        exit()
+    fps = cap.get(cv.CAP_PROP_FPS)
 
+    # Opening cascade classifier
+    cascade_path = 'old_cascades/cascade_'+str(cascade_num)+'/cascade.xml'
+    # cascade_wildfire = cv.CascadeClassifier('cascade/cascade.xml')
+    cascade_wildfire = cv.CascadeClassifier(cascade_path)
 
-if not cap.isOpened():
-    print("Cannot open capture")
-    exit()
-fps = cap.get(cv.CAP_PROP_FPS)
+    # Resize each image to this width and height
+    width = 480
+    height = 360
+    dim = (width, height)
 
-# Opening cascade classifier
-# cascade_wildfire = cv.CascadeClassifier('cascade/cascade.xml')
-cascade_wildfire = cv.CascadeClassifier('old_cascades/cascade_017/cascade.xml')
+    loop_time = time()
+    while True:
+        ret, frame = cap.read()
+        if frame is None:
+            print('--(!) No captured frame -- Break!')
+            break
+        # frame = cv.resize(frame, dim, interpolation = cv.INTER_AREA)
 
-# Resize each image to this width and height
-width = 480
-height = 360
-dim = (width, height)
+        # Testing out cascade classifier
+        rectangles = cascade_wildfire.detectMultiScale(frame)
+        detection_image = draw_rectangles(frame, rectangles)
+        cv.imshow('Cal Poly WEDS Fire Detection', frame)
 
-loop_time = time()
-while True:
-    ret, frame = cap.read()
-    if frame is None:
-        print('--(!) No captured frame -- Break!')
-        break
-    # frame = cv.resize(frame, dim, interpolation = cv.INTER_AREA)
+        key = cv.waitKey(1)
+        if key == ord('q'):
+            cv.destroyAllWindows()
+            break
 
-    # Testing out cascade classifier
-    rectangles = cascade_wildfire.detectMultiScale(frame)
-    detection_image = draw_rectangles(frame, rectangles)
-    cv.imshow('Cal Poly WEDS Fire Detection', frame)
+        # For generating image samples
+        # elif key == ord('f'):
+        #     print("Saving positive image!")
+        #     cv.imwrite('positive/{}.jpg'.format(loop_time), frame)
+        # elif key == ord('d'):
+        #     print("Saving negative image!")
+        #     cv.imwrite('negative/{}.jpg'.format(loop_time), frame)
+        # elif key == ord('g'):
+        #     print("Saving test image!")
+        #     cv.imwrite('tests/{}.jpg'.format(loop_time), frame)
 
-    key = cv.waitKey(1)
-    if key == ord('q'):
-        cv.destroyAllWindows()
-        break
-
-    # For generating image samples
-    # elif key == ord('f'):
-    #     print("Saving positive image!")
-    #     cv.imwrite('positive/{}.jpg'.format(loop_time), frame)
-    # elif key == ord('d'):
-    #     print("Saving negative image!")
-    #     cv.imwrite('negative/{}.jpg'.format(loop_time), frame)
-    # elif key == ord('g'):
-    #     print("Saving test image!")
-    #     cv.imwrite('tests/{}.jpg'.format(loop_time), frame)
+if __name__ == '__main__':
+    main()
